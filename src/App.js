14c10384ad52ca66a8190144d7471b8030544f16
Karthik Gumpu <karthik.gumpu@netenrich.com>
2020-11-23 23:05:53 +0530

    Change primary theme color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
 import AppContainer from "./components/AppContainer";
+import theme from "./utils/theme";
 
-const App = () => <AppContainer />;
+const App = () => (
+  <MuiThemeProvider theme={theme}>
+    <AppContainer />
+  </MuiThemeProvider>
+);
 
 export default App;
---
+++ src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "rgb(237, 90, 107)",
+      main: "rgb(237, 90, 107)",
+      dark: "rgb(237, 90, 107)",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default theme;
