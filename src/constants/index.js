b80eaaf05a169b9a70ba39d4a5d3e42a2849288a
Karthik Gumpu <karthik.gumpu@netenrich.com>
2020-11-24 00:58:10 +0530

    Add api utility

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,6 @@
+export const API_HOST = "https://developers.zomato.com/api/";
+export const USER_KEY = "07409b4e20bae0709ea4ff8d9f8b8a2c";
+
 // Keeping random keys in localstorage just to divert while hacking
 export const ZOMATO_LOCAL_STORAGE = {
   STORAGE_NAME: "ADsewKewr3DAgtdASw2J",
---
+++ src/utils/api.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { v4 as uuid } from "uuid";
+import Promise from "bluebird";
+import constants from "../constants";
+
+Promise.config({
+  cancellation: true,
+});
+
+const getFullUrl = (url) => {
+  return `${constants.API_HOST}${url}`;
+};
+
+const simpleAxiosInstance = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+    "user-key": constants.USER_KEY,
+  },
+});
+
+class Api {
+  constructor() {
+    this.call = {};
+  }
+
+  doPromise = (axiosInstance, config, requestName) => {
+    const requestPromise = new Promise((resolve, reject, onCancel) => {
+      axiosInstance(config)
+        .then(
+          (success) => {
+            resolve(success.data);
+          },
+          (error) => {
+            if (axios.isCancel(error)) {
+              console.log(`Request Aborted for ${config.url}`);
+            } else if (error.response) {
+              reject(error.response.data);
+            } else {
+              reject(error);
+            }
+          }
+        )
+        .catch((error) => {
+          reject(error);
+        });
+      onCancel(() => {
+        this.call[requestName].cancel();
+      });
+    });
+    return requestPromise;
+  };
+
+  request = (config = {}) => {
+    const requestName = uuid();
+    this.call[requestName] = axios.CancelToken.source();
+    config.cancelToken = this.call[requestName].token;
+    config.url = getFullUrl(config.url);
+    return this.doPromise(simpleAxiosInstance, config, requestName);
+  };
+}
+
+// Utility method to cancel inprogress apis on need basis
+const cancelApis = (apis = [], msg = "") => {
+  for (let i = 0; i < apis.length; i++) {
+    if (apis[i] && apis[i].cancel) {
+      apis[i].cancel(msg);
+    }
+  }
+};
+
+const api = new Api();
+
+export { cancelApis };
+export default api;
